772a0af18fdbdbeee78309156905584fc90d3df1
Mahi130594 <enugurthymahendra@gmail.com>
2025-05-10 12:09:59 +0530

    Remove hardcoded token

--- check_commits.py
+++ check_commits.py
@@ -5,7 +5,7 @@ import os
 
 REPO = "yourusername/html-ci-project"
 BRANCH = "master"
-TOKEN = "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"  # created a personal access token with repo access
+TOKEN = "ghp_****"  # created a personal access token with repo access
 LAST_COMMIT_FILE = "/home/mahi/HTML-Project/last_commit.txt"
 
 headers = {"Authorization": f"token {TOKEN}"}
